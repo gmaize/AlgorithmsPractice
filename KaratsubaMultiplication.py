2a784d49ee92e09b73e15dc45f06106a83d6a20e
Gianni Maize <gmaize@alumni.stanford.edu>
2017-12-20 14:49:27 -0800

    removed call to karatsubaMultiply

--- KaratsubaMultiplication.py
+++ KaratsubaMultiplication.py
@@ -1,4 +1,3 @@
-
 def karatsubaMultiply(x, y):
 	if x == 0 or y == 0:
 		return 0
@@ -23,6 +22,3 @@ def karatsubaMultiply(x, y):
 	crossProduct = karatsubaMultiply((a+b), (c+d))
 	midterm = crossProduct - ac - bd
 	return (10**(2*nby2)*ac) + (10**nby2) * midterm + bd
-
-result = karatsubaMultiply(3141592653589793238462643383279502884197169399375105820974944592, 2718281828459045235360287471352662497757247093699959574966967627)
-print(result)
